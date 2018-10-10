1bd905b999f289f3f32f3f8c5b00c2c4aa6860ec
nintendavid26 <nintendavid26@aol.com>
2018-10-09 20:37:33 -0400

    Added Menus

--- Assets/Scripts/Ship/Door.cs
+++ Assets/Scripts/Ship/Door.cs
@@ -7,13 +7,12 @@ namespace Sink {
 
 		public Room room1, room2;
 
-		protected override void DoAction(Player p){
-			if (p.curRoom == room1){
-				p.EnterRoom(room2,this);
-			}
-			else if(p.curRoom == room2){
-				p.EnterRoom(room1,this);
+		protected override void DoAction(Player p) {
+			if (p.curRoom == room1) {
+				p.EnterRoom(room2, this);
+			} else if (p.curRoom == room2) {
+				p.EnterRoom(room1, this);
 			}
 		}
-    }
+	}
 }
