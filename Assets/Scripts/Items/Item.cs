46363949089db62d8d99671a5cd3ac9abbc45929
Ben Filbert <bf372@drexel.edu>
2018-10-18 00:18:13 -0400

    - Added items, which the player can interact with. Once they do, the object de-spawns and a representation of it is added to their inventory via dictionary entry. -  Planned additions : Repair objects which de-spawn when the player interacts with them when enough resources are in inventory. Random Spawn points where the resource items end up when a session begins.

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//
 namespace Sink {
 	public class Item {
 
 		public string Name;
 
-		public Item() {
-
+		public Item()
+		{}
+		public Item(string name) {
+			Name = name;
 		}
+
+		public Item(Item x)
+		{
+			Name = x.Name;
+		}
+
 	}
 }
