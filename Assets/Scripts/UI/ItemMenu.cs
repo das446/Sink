4cbeb466e260effd8bcd323fd00849490075ec3a
nintendavid26 <nintendavid26@aol.com>
2018-10-18 12:38:06 -0400

    Changed some things menus

--- Assets/Scripts/UI/ItemMenu.cs
+++ Assets/Scripts/UI/ItemMenu.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 namespace Sink {
 	public class ItemMenu : MonoBehaviour,IMenu {
 
+		
+
 		public void Close(LocalPlayer p) {
 			gameObject.SetActive(false);
 		}
 
 		public void Open(LocalPlayer p) {
 			gameObject.SetActive(true);
+
+			//Get items from player
+			//You have a grid
+			//put the sprites into a grid
 		}
 	}
 }
