6be1d45bb3f8bdc7a909e426d962fd767cf07a3a
nintendavid26 <nintendavid26@aol.com>
2018-10-21 19:24:55 -0400

    Making items work with networking

--- Assets/Scripts/Items/ItemSpawner.cs
+++ Assets/Scripts/Items/ItemSpawner.cs
@@ -12,12 +12,17 @@ namespace Sink {
 
 		public ItemInteractable baseItem;
 
+		void Start(){
+			Item i = possibleItems.RandomItem();
+			Room r = rooms.RandomItem();
+			SpawnItem(i,r);
+		}
+
 		public void SpawnItem(Item item, Room room) {
 			Vector3 location = room.possibleSpawnLocations.RandomItem();
-
-			
 			ItemInteractable i = Instantiate(baseItem,location,Quaternion.identity);
 			i.Initialize(item,location);
+			NetworkServer.Spawn(i.gameObject);
 
 
 		}
