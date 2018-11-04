bf6205afa87ce8eb7d75818848c70c2df61ed6db
Ben Filbert <bf372@drexel.edu>
2018-11-04 13:39:09 -0500

    >Created/Working on Item_Search, Item_Spawn_Controller, and ItemSpawner. My current idea is that the script Item_Spawn_Controller is attached to the network host and calls a specific new function in the ItemSpawner and passes a list of the new item_search objects (props that have a progress bar and a search time which then give the player an item). > Due to not knowing how to attach the Item_Spawn_controller script to the network host, bug testing to see if the code functions at all has been problematic.

---
+++ Assets/Scripts/Items/Item_Spawn_Controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace Sink{
+public class Item_Spawn_Controller : NetworkBehaviour {
+
+	// Attach to host 
+	// To prevent sever from spawning mutliple objects in the same space
+
+	
+		List<Item_Search> Item_props;
+
+		public static ItemSpawner singleton; //use editor to set this 
+
+		void  Start()
+		{
+			singleton.Host_Called(Item_props); // calls the singleton to spawn objects when host finishes loading in
+			// Attempt to reuse already prexisting itemspawner script, now with new function.
+		}
+}
+}
