be83ff314c03c672638487296c50061e7d3b20c3
louislee95 <ll579@drexel.edu>
2018-11-05 03:56:14 -0500

    Added various menus (Main, Pause, Options, Controls, Sounds, Parameters), game objects/controllers, and scripts to go with the menus. I did not have access to the actual characters and other assets used during the demo/UNET scene so I'm not sure if most of my code actually works, it is coded hypothetically. Parameters are as follows: Sounds (player sound, sfx, music, master), Controls (change mouse sensitivity, invert x and y axes), Change Player X,Y, and, Z positions, change door opening speed (to be implemented since there are no door opening and closing animations), change ladder climbing speed (to be implemented since idk if climbing has been coded in), change item pick up rate, change player speed. Again, sorry this took so much longer than it had any right to be.

---
+++ Assets/Scripts/Player/PlayerSpeedController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Sink;
+
+public class PlayerSpeedController : Player {
+	
+	//idk if inheritence is needed, but better to be safe than sorry
+
+	public float moveSpeed = 2;
+	public Slider speedSlider;
+
+
+	public void ChangePlayerSpeed(float newSpeed)
+	{
+		moveSpeed = newSpeed;
+		newSpeed = speedSlider.value;
+		Debug.Log("Character speed value is :" + speedSlider.value);
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/UI/MapMenu.cs
+++ Assets/Scripts/UI/MapMenu.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Sink {
+namespace Sink 
+{
 	public class MapMenu : MonoBehaviour, IMenu {
 		public void Close(LocalPlayer p) {
 			gameObject.SetActive(false);
