f6120cf349472e7023552a0923a100186910b4fc
nintendavid26 <nintendavid26@aol.com>
2018-11-08 11:59:36 -0500

    Added some scripts to be added to later

--- Assets/Scripts/Helpers/ShipEvents/ShipEvent.cs
+++ Assets/Scripts/Helpers/ShipEvents/ShipEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class ShipEvent : MonoBehaviour {
+public abstract class ShipEvent : ScriptableObject {
 
 	public float activationTime;
 
