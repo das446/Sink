dab082072701025ed3f7131e604e6f3283fd00c8
nintendavid26 <nintendavid26@aol.com>
2018-11-08 13:07:55 -0500

    Changed the way events work

--- Assets/Scripts/Helpers/ShipEvents/ShipEvent.cs
+++ Assets/Scripts/Helpers/ShipEvents/ShipEvent.cs
@@ -2,9 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class ShipEvent : ScriptableObject {
-
-	public float activationTime;
+public abstract class ShipEvent : MonoBehaviour {
 
 	public abstract void Activate();
 
