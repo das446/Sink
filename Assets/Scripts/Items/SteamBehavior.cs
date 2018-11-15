64922940c278eae9bfb9c8e814e407d06854d9d9
louislee95 <ll579@drexel.edu>
2018-11-15 10:08:22 -0500

    Added kda to play in the background. Volume increases and decreases based on distance from kda gameobject location (0,0,0). Also created a scenario where if the player enters the room to the left of the starting room, smoke will appear

--- Assets/Scripts/Items/SteamBehavior.cs
+++ Assets/Scripts/Items/SteamBehavior.cs
@@ -2,18 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SteamBehavior : MonoBehaviour {
+namespace Sink
+{
+public class SteamBehavior : MonoBehaviour
+ {
 
-	public ParticleSystem smoke;
+	public GameObject smoke;
+	public GameObject pipe;
+	Room room;
+	//bool isOn = true;
+   //bool enter = true;
 	
-
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		smoke.gameObject.SetActive(false);
+		//smoke.GetComponent<ParticleSystem>().enableEmission = false;
+		//gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
+
+	public void OnTriggerEnter(Collider c)
+	{
+        if(c.GetComponent<Player>())
+        {
+            smoke.gameObject.SetActive(true);
+        }
+        /*
+		if(c.tag == "Player")
+		{
+			smoke.Play();
+			Debug.Log("You have activated the smoke");
+		}
+*?
+        /*
+		if(c.GetComponent<Collider>().tag == "Player")
+		{
+			c.gameObject.GetComponent<ParticleSystem>().Play();
+		}
+        */
+		//smoke.GetComponent<ParticleSystem>().enableEmission = true;
+		
+
+		/*
+		if(enter)
+		{
+			GetComponent<ParticleSystem>().Play();
+			ParticleSystem.EmissionModule em = GetComponent<ParticleSystem>().emission;
+			em.enabled = true;
+			Debug.Log("Smoke will activate");
+		}
+		*/
+	}
+}
 }
