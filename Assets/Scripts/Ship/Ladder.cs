a942ce8649f235d0f48aa34cbb563fb6b457c140
nintendavid26 <nintendavid26@aol.com>
2018-11-28 15:50:13 -0500

    Added random event component and smoke event

--- Assets/Scripts/Ship/Ladder.cs
+++ Assets/Scripts/Ship/Ladder.cs
@@ -8,44 +8,18 @@ namespace Sink {
 		public Room upperRoom, lowerRoom;
 		public Floor upperFloor, lowerFloor;
 		public Transform top, bottom;
-		
+
 		//Temporary rng system
-		public int rng;
-		public GameObject smoke;
+
 		public GameObject pipe;
 
-		void Start()
-		{
-			rng = Random.Range(0,4);
-		}
-
-		public override void DoAction(Player p)
-		 {
-			if (p.curFloor == lowerFloor && rng == 0) 
-			{
+		public override void DoAction(Player p) {
+			if (p.curFloor == lowerFloor) {
 				p.StartCoroutine(p.ClimbLadder(this, upperRoom, upperFloor));
-				Debug.Log("Smoke activated. RNG = " + rng);
-				smoke.gameObject.SetActive(true);
-			}
-			 else if(p.curFloor == lowerFloor && (rng == 1 || rng == 2 || rng == 3))
-			 {
-				p.StartCoroutine(p.ClimbLadder(this, upperRoom, upperFloor));
-				Debug.Log("Smoke not activated. RNG = " + rng);
-				smoke.gameObject.SetActive(false);				 
-			 }
-			 else if(p.curFloor == upperFloor && rng == 0)
-			 {
+			} else if (p.curFloor == upperFloor) {
 				p.StartCoroutine(p.ClimbLadder(this, lowerRoom, lowerFloor));
-				Debug.Log("Smoke activated. RNG = " + rng);
-				smoke.gameObject.SetActive(true);
-			 }
-			 else if(p.curFloor == upperFloor && (rng == 1 || rng == 2 || rng == 3)) 
-			 {
-				p.StartCoroutine(p.ClimbLadder(this, lowerRoom, lowerFloor));
-				Debug.Log("Smoke not activated. RNG = " + rng);
-				smoke.gameObject.SetActive(false);
 			}
-			
+			CheckRandomTrigger();
 		}
 	}
 }
