cf3af962a5ef3253f54025f32a46657d96db0ba4
louislee95 <ll579@drexel.edu>
2018-11-28 13:41:59 -0500

    Added RNG element to pipe burst. Smoke effect activates 25% of the time depending on RNG. Will activate when climbing ladders and in a room randomly. Could not get it to work when searching for an item. Added sound effects (needs to be implemented I think) and music to the game. Added an inventory background but the background appears in front of the image.

--- Assets/Scripts/Ship/Ladder.cs
+++ Assets/Scripts/Ship/Ladder.cs
@@ -8,12 +8,42 @@ namespace Sink {
 		public Room upperRoom, lowerRoom;
 		public Floor upperFloor, lowerFloor;
 		public Transform top, bottom;
+		
+		//Temporary rng system
+		public int rng;
+		public GameObject smoke;
+		public GameObject pipe;
 
-		public override void DoAction(Player p) {
-			if (p.curFloor == lowerFloor) {
+		void Start()
+		{
+			rng = Random.Range(0,4);
+		}
+
+		public override void DoAction(Player p)
+		 {
+			if (p.curFloor == lowerFloor && rng == 0) 
+			{
 				p.StartCoroutine(p.ClimbLadder(this, upperRoom, upperFloor));
-			} else if(p.curFloor == upperFloor) {
+				Debug.Log("Smoke activated. RNG = " + rng);
+				smoke.gameObject.SetActive(true);
+			}
+			 else if(p.curFloor == lowerFloor && (rng == 1 || rng == 2 || rng == 3))
+			 {
+				p.StartCoroutine(p.ClimbLadder(this, upperRoom, upperFloor));
+				Debug.Log("Smoke not activated. RNG = " + rng);
+				smoke.gameObject.SetActive(false);				 
+			 }
+			 else if(p.curFloor == upperFloor && rng == 0)
+			 {
 				p.StartCoroutine(p.ClimbLadder(this, lowerRoom, lowerFloor));
+				Debug.Log("Smoke activated. RNG = " + rng);
+				smoke.gameObject.SetActive(true);
+			 }
+			 else if(p.curFloor == upperFloor && (rng == 1 || rng == 2 || rng == 3)) 
+			 {
+				p.StartCoroutine(p.ClimbLadder(this, lowerRoom, lowerFloor));
+				Debug.Log("Smoke not activated. RNG = " + rng);
+				smoke.gameObject.SetActive(false);
 			}
 			
 		}
