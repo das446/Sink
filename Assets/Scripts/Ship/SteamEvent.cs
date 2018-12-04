58c2a93bbd295560a2500da8392a5fa5ab945003
nintendavid26 <nintendavid26@aol.com>
2018-12-03 21:33:22 -0500

    Added pipe burst event, still need to make steam look better

---
+++ Assets/Scripts/Ship/SteamEvent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Sink {
+	public class SteamEvent : ShipEvent {
+
+		public Transform t;
+		public GameObject smoke;
+		public SteamPipe pipe;
+
+		public bool active;
+		public float timer;
+		public float baseTime;
+
+		public override void Activate() {
+			Debug.Log("Make Smoke");
+			smoke.SetActive(true);
+			smoke.transform.position = pipe.steamSpawn.position;
+			pipe.steamEvent = this;
+			active = true;
+			timer = baseTime;
+			//Send chat alert
+		}
+
+		public void Stop() {
+			smoke.SetActive(false);
+			active=false;
+
+		}
+
+		public void Update() {
+			if (active) {
+				timer -= Time.deltaTime;
+				if (timer <= 0 && isServer) {
+					Player.EveryoneLoses();
+				}
+			}
+
+		}
+
+	}
+}
