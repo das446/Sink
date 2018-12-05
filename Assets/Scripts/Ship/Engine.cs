4638befb1b238068ee5a27b316c3ff2eeb592067
nintendavid26 <nintendavid26@aol.com>
2018-12-05 08:59:03 -0500

    Implemented new character animations and hooked it up to controller

--- Assets/Scripts/Ship/Engine.cs
+++ Assets/Scripts/Ship/Engine.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Sink {
-	public class Engine : Interactable {
+	public class Engine : Interactable { 
 
-		public Item battery;
-
-		public override void DoAction(Player p){
-			if(p.curFloor.oxygen.curOx>0 && SteamEvent.activeEvents.Count==0){
+		public override void DoAction(Player p) {
+			if (p.curFloor.oxygen.curOx > 0 && SteamEvent.activeEvents.Count == 0) {
 				//p.inventory.UseItem(battery);
 				p.Win();
 			}
