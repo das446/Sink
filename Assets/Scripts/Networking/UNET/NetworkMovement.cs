481c1f3af2d7a6bbc5308e76e1d407cd03e06eb4
nintendavid26 <nintendavid26@aol.com>
2018-12-12 22:10:16 -0500

    fixed floating and lock lights

--- Assets/Scripts/Networking/UNET/NetworkMovement.cs
+++ Assets/Scripts/Networking/UNET/NetworkMovement.cs
@@ -24,7 +24,7 @@ namespace Sink {
 			player.animator.SetSpeed(move);
 			transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime * latencySmoothingFactor);;
 			if (lookWhereGoing) {
-				Vector3 newTarget = new Vector3(target.x, transform.position.y, target.z);
+				Vector3 newTarget = new Vector3(target.x,target.y, target.z);
 				transform.LookAt(newTarget);
 			} else {
 				transform.rotation.eulerAngles.Set(0, rotY, 0);
